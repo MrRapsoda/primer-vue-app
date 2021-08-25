3cb1f65474bbf5dfa4fc1f1b0b1e7eeb37212a9d
mrrapsoda <dragonsflames50@gmail.com>
2021-08-24 21:35:17 -0600

    sync

---
+++ src/components/Counter.vue
@@ -0,0 +1,46 @@
+<template>
+  <h2>{{ customTitle }}</h2>
+  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+  <div>
+      <button @click=" incrementCounter ">+1</button>
+      <button @click=" decrementCounter ">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['title'],
+    data() { 
+        return {
+            counter: 20
+        }
+    },
+    computed: {
+        squareCounter() { 
+            return this.counter * this.counter;
+        },
+        customTitle() { 
+            return this.title ? this.title : 'No title provided';
+        }
+    },
+    methods: {
+        incrementCounter() { 
+            this.counter += 1;
+        },
+        decrementCounter() { 
+            this.counter -= 1;
+        }
+    }
+}
+</script>
+
+<style>
+button {
+    margin: 10px;
+    padding: 5px 15px;
+    background: #41B883;
+    border: none;
+    color: white;
+    border-radius:10px
+}
+</style>
